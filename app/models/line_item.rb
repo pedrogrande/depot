af265981e841b76fa461bd0be2b5fdee507dc81f
pedrogrande <peter.argent@gmail.com>
2013-04-30 12:22:15 +1000

    ajax1

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
-  attr_accessible :product
+  attr_accessible :product_id
 
  def total_price 
  	product.price * quantity
